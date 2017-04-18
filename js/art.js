926555965af391ec40c1d1d079bca7dea9b4c5f4
Alan Jordan <alanpjordan@gmail.com>
2017-04-18 19:26:40 +1200

    Got random color generator working

--- js/art.js
+++ js/art.js
@@ -1,1 +1,33 @@
 // Need event listeners on all buttons
+document.onload = initialiseButtons();
+
+function initialiseButtons() {
+  document.getElementById("one").addEventListener("click", changeBackgroundColour);
+  document.getElementById("two").addEventListener("click", changeBackgroundColour);
+  document.getElementById("three").addEventListener("click", changeBackgroundColour);
+  document.getElementById("four").addEventListener("click", changeBackgroundColour);
+  document.getElementById("five").addEventListener("click", changeBackgroundColour);
+  document.getElementById("six").addEventListener("click", changeBackgroundColour);
+  document.getElementById("seven").addEventListener("click", changeBackgroundColour);
+  document.getElementById("eight").addEventListener("click", changeBackgroundColour);
+  document.getElementById("nine").addEventListener("click", changeBackgroundColour);
+  document.getElementById("ten").addEventListener("click", changeBackgroundColour);
+  document.getElementById("eleven").addEventListener("click", changeBackgroundColour);
+  document.getElementById("twelve").addEventListener("click", changeBackgroundColour);
+}
+
+
+
+function changeBackgroundColour() {
+  var id = event.target.id;
+  document.getElementById(id).style.backgroundColor = randomRGB();
+  document.getElementById(id).style.color = randomRGB();
+  document.body.style.backgroundColor = randomRGB();
+}
+
+function randomRGB(){
+  var colOne = Math.round(Math.random() * 255);
+  var colTwo =  Math.round(Math.random() * 255);
+  var colThree =  Math.round(Math.random() * 255);
+  return "rgb("+ colOne +","+ colTwo+","+colThree+")";
+}
