8f7e41c939daf18123b74712f6898a1b1e4f8272
Alan Jordan <alanpjordan@gmail.com>
2017-04-18 19:52:08 +1200

    Random width generator

--- js/art.js
+++ js/art.js
@@ -23,8 +23,9 @@ function randomiseButton() {
   document.getElementById(id).style.backgroundColor = randomRGB();
   document.getElementById(id).style.color = randomRGB();
   document.getElementById(id).style.fontSize = randomFontSize();
-  document.getElementById(id).style.margin = randomMarginBorder();
-  document.getElementById(id).style.padding = randomMarginBorder();
+  document.getElementById(id).style.margin = randomMarginPadding();
+  document.getElementById(id).style.padding = randomMarginPadding();
+  document.getElementById(id).style.width = randomWidth();
   document.getElementById(id).style.borderRadius = randomBorderRadius();
   document.body.style.backgroundColor = randomRGB();
 }
@@ -40,10 +41,13 @@ function randomFontSize() {
   return Math.round(Math.random() * 200) + "%";
 }
 
-function randomMarginBorder() {
+function randomMarginPadding() {
   return Math.round(Math.random() * 25) + "%";
 }
 
 function randomBorderRadius() {
   return Math.round(Math.random() * 50) + "px";
+}
+function randomWidth() {
+  return Math.round(Math.random() * 300) + "px";
 }
